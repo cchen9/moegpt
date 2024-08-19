c16211e5fbaf01024e75821405d97eb144e9fd65
Ubuntu <ubuntu@ip-10-162-0-78.us-west-2.compute.internal>
2024-08-19 20:09:05 +0000

    more running

--- chargpt.py
+++ chargpt.py
@@ -121,6 +121,8 @@ if __name__ == '__main__':
             model.eval()
             with torch.no_grad():
                 # sample from the model...
+                import pdb
+                pdb.set_trace()
                 context = "O God, O God!"
                 x = torch.tensor([train_dataset.stoi[s] for s in context], dtype=torch.long)[None,...].to(trainer.device)
                 y = model.generate(x, 500, temperature=1.0, do_sample=True, top_k=10)[0]
